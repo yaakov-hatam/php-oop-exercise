3124e736b877b532ab2ad093065a38a4ba8edbae
Your Name <you@example.com>
2020-03-04 19:02:43 +0200

    fix tester

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ $pur4 = new purchase($screen2, $mouse1, $keyboard2, $computer2);
 echo $screen1->getSpecs()."\n"; // Screen manufacturer: Samsung, model: S24F350FH, panel: PLS, Price: 723
 echo $mouse2->getSpecs()."\n"; // Mouse manufacturer: LogiTech, model: MX Master, Price: 799
 echo $computer3->getSpecs()."\n"; // Computer manufacturer: Intel, model: , Price: 1580
-echo $keyboard1->getSpecs()."\n"; // Keyboard manufacturer: Microsoft, Price: 325
+echo $keyboard1->getSpecs()."\n"; // Keyboard manufacturer: Microsoft, model: Natural Ergonomic 4000, isWired: 1, Price: 325
 
 echo $pur1->getFullPurchaseDetails()."\n";
 echo $pur2->getFullPurchaseDetails()."\n";
@@ -66,22 +66,22 @@ echo $pur4->getFullPurchaseDetails()."\n";
 /* 
     Screen manufacturer: Samsung, model: S24F350FH, panel: PLS, Price: 723
     Mouse manufacturer: Microsoft, model: Mobile Mouse 1850, Price: 129
-    Keyboard manufacturer: Microsoft, Price: 325
+    Keyboard manufacturer: Microsoft, model: Natural Ergonomic 4000, isWired: 1, Price: 325
     Computer manufacturer: GIGABYTE, model: , Price: 3059
 
     Screen manufacturer: Samsung, model: C27F591FD, panel: VA, Price: 2074
     Mouse manufacturer: LogiTech, model: M185, Price: 128
-    Keyboard manufacturer: Microsoft, Price: 325
+    Keyboard manufacturer: Microsoft, model: Natural Ergonomic 4000, isWired: 1, Price: 325
     Computer manufacturer: Intel, model: , Price: 1580
 
     Screen manufacturer: Philips, model: 273V5LHAB, panel: TFT, Price: 1517
     Mouse manufacturer: LogiTech, model: MX Master, Price: 799
-    Keyboard manufacturer: Corsair, Price: 799
+    Keyboard manufacturer: Corsair, model: K95, isWired: 1, Price: 799
     Computer manufacturer: GIGABYTE, model: , Price: 3059
 
     Screen manufacturer: Samsung, model: S22F350FH, panel: TN, Price: 620
     Mouse manufacturer: Microsoft, model: Mobile Mouse 1850, Price: 129
-    Keyboard manufacturer: Logitech, Price: 285
+    Keyboard manufacturer: Logitech, model: G105, isWired: 1, Price: 285
     Computer manufacturer: AMD, model: , Price: 2418
 
 */
